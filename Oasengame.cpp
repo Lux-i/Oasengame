68f521c4a1bb5905a7d8ab472403bc78de00562a
LuxorUWU <s-s@s.com>
2025-02-26 21:07:09 +0100

    mainloop rewrite fixed gameLog changed input getting

--- Oasengame.cpp
+++ Oasengame.cpp
@@ -1,29 +1,53 @@
 #include <iostream>
 #include <vector>
 #include <cstdlib>
+#include <string>
 #include "Board.h"
 #include "Player.h"
 
+constexpr char EXIT_COMMAND = 'x';
+
+#ifdef _WIN32
+const char* CLEAR_SCREEN = "cls";
+#else
+const char* CLEAR_SCREEN = "clear";
+#endif
+
 int main()
 {
-    Board board = Board();
-    Player player = Player();
+    Board board;
+    Player player;
     std::vector<std::string> gameLog;
-    char input = 'x';
+    std::string inputStr;
+    char input;
 
     //game-loop start
     do
     {
-        std::system("cls");
+#pragma region renderScreen
+        std::system(CLEAR_SCREEN);
         board.renderBoard(player.position);
         std::cout << "\nHealth: " << player.getHealth() << " | Relics: " << player.getRelics();
         std::cout << "\n";
-        for (std::string m : gameLog) {
-            std::cout << m << "\n";
+
+        //show last 10 entries in the log
+        int start = std::max(0, (int)gameLog.size() - 10);
+        for (int i = start; i < gameLog.size(); ++i) {
+            std::cout << gameLog[i] << "\n";
+        }
+#pragma endregion
+
+        //get input
+        std::getline(std::cin, inputStr);
+        if (!inputStr.empty()) {
+            input = inputStr[0];
+            gameLog.push_back("Pressed " + std::string(1, input));
+        } else {
+            //set input to a char that executes no action in case
+            input = 'o';
         }
 
-        std::cin >> input;
-        gameLog.push_back("Pressed " + input);
+        //handle Input
 
-    } while (input != 'x');
+    } while (input != EXIT_COMMAND);
 }
